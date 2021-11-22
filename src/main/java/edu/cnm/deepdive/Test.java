3ec401196122d9d933751191b46591095dbbf71f
Royce Reano <reano.royce@gmail.com>
2021-11-22 11:04:35 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/Test.java
@@ -0,0 +1,33 @@
+package edu.cnm.deepdive;
+
+import edu.cnm.deepdive.model.Card;
+import edu.cnm.deepdive.model.Rank;
+import edu.cnm.deepdive.model.Suit;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Test {
+
+  public static void main(String[] args) {
+    List<Card> deck = new ArrayList<>();
+    for (Suit s : Suit.values()) {
+      for (Rank r : Rank.values()) {
+        Card c = new Card(r, s);
+        deck.add(c);
+      }
+    }
+    System.out.println(deck);
+    Collections.shuffle(deck, new SecureRandom());
+    System.out.println(deck);
+    Collections.sort(deck);
+    System.out.println(deck);
+    int numRanks = Rank.values().length;
+    deck.sort(Comparator.comparingInt((card) ->
+        (card.getRank().ordinal() + numRanks - 1) % numRanks));
+    System.out.println(deck);
+  }
+
+}
---
+++ src/main/java/edu/cnm/deepdive/model/Card.java
@@ -0,0 +1,35 @@
+package edu.cnm.deepdive.model;
+
+public class Card implements Comparable<Card> {
+
+  private final Rank rank;
+  private final Suit suit;
+
+  public Card(Rank rank, Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  public Rank getRank() {
+    return rank;
+  }
+
+  public Suit getSuit() {
+    return suit;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s %s", rank.getSymbol(), suit.getSymbol());
+  }
+
+  @Override
+  public int compareTo(Card other) {
+    int comparison = suit.compareTo(other.suit);
+    if (comparison == 0) {
+      comparison = rank.compareTo(other.rank);
+    }
+    return comparison;
+  }
+
+}
